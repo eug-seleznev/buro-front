69d21ee54eec84fdfcfd6306d41ffc158592e08d
Mikteor <dm.pustovitenko@gmail.com>
2021-01-18 11:49:54 +0200

    Container, Card styled components

--- my-app/src/Styles/common.js
+++ my-app/src/Styles/common.js
@@ -2,11 +2,30 @@ import styled from 'styled-components'
 
 
 export const Container =  styled.div`
-margin-left: 10vw;
-width: 80vw;
-margin-top: 20vh;
+padding-left: 350px;
+padding-right: 154px;
+width: 100%;
+height: 100%;
+padding-top: 200px;
+padding-bottom: 200px;
+background-color: #ECECEC;
+
 
 `
+
+export const Card = styled.div`
+background: #FFFFFF;
+
+padding: 15px;
+padding-left:34px;
+border: 1px solid #CECECE;
+border-radius: 15px;
+`
+
+
+
+
+// не используем, не удалял, чтоб не ломались импорты старые, вся типография переносится в typography.js
 export const SmallContainer =  styled.div`
 margin-left: 30vw;
 width: 40vw;
@@ -19,11 +38,7 @@ export const Title =  styled.h1`
     align-items: center;
   
 `
+// /////////////////////////////////////////////
 
 
-export const Card = styled.div`
-background-color:white;
-filter: drop-shadow(0 0 5px black);
-padding: 15px;
-`
 
