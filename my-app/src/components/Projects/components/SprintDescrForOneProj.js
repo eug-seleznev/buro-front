f0496e34946f77bd435e872aad532099c8a0fab3
alexdcrv <bloodlydeadknight@gmail.com>
2021-01-19 14:16:49 +0300

    pull

--- my-app/src/components/Projects/components/SprintDescrForOneProj.js
+++ my-app/src/components/Projects/components/SprintDescrForOneProj.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { Button } from "../../../Styles/buttons"
 import { Card } from "../../../Styles/common"
 import './sprintdescr.css'
-
+import style from '../../../Styles/modules/components/Project/oneproj.module.css'
 
 const SprintDescription = ({sprintname, index, dateOpen,taskcomplite, alltasks, history, params, id}) => {
 	const [loaded, setLoaded] = useState (0)
@@ -24,20 +24,24 @@ const SprintDescription = ({sprintname, index, dateOpen,taskcomplite, alltasks, 
 
 	return (
 	<>
-		{!loaded?<div>loading...</div>:(<Card>
-			<div className='sprintdescr__grid__container'>
+		{!loaded?<div>loading...</div>:(
+		<div className={style.card}>
+		
+			<Card>
+			
 				<div>{dateOpen.slice(5,10).replace(/-/g, ".")}-01.??</div>
 				<div>Спринт №{index}</div>
 				<div>Короткое описание</div>
-				<div>До дедлайна: {diff.toString().slice(0,1)} д.</div>
+				<div>Дней до дедлайна: {diff.toString().slice(0,1)}</div>
 				<div>Задачи {taskcomplite}/{alltasks} </div>
 				<div>
 					<Button fontSize={'16px'} grey>Добавить в избранное</Button>
 					<Button fontSize={'16px'} onClick={() => history.push(`/projects/${params}/${id}`)}>Подробнее</Button>
 				</div>
+				</Card>
 			</div>
 			
-		</Card>)}
+		)}
 		</>
 	)
 }
