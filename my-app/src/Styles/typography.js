c6b7b1b2cb407a2f2ff9a32954a9488a9470bac7
Mikteor <dm.pustovitenko@gmail.com>
2021-01-19 12:31:26 +0200

    typ

--- my-app/src/Styles/typography.js
+++ my-app/src/Styles/typography.js
@@ -1,7 +1,30 @@
 import styled from 'styled-components'
 
 
+export const Bold = styled.h1`
+margin: 0;
+padding: 0;
+font-family: SuisseIntlSemiBold;
+font-size: ${props => props.size}px;
+color: ${props => props.color};
+`
+export const Light = styled.p`
+margin: 0;
+padding: 0;
+font-family: SuisseIntlRegular;
+font-size: ${props => props.size}px;
+color: ${props => props.color};
+`
+export const Thin = styled.p`
+margin: 0;
+padding: 0;
+font-family: SuisseIntlLight;
+font-size: ${props => props.size}px;
+color: ${props => props.color};
+`
 
+
+// not in use///////////////////
 export const H1 = styled.h1`
 margin:0;
 margin-bottom: 0px;
